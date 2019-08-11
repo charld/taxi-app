23e2025643ec41b757483e91a994d6dd3b6ace7c
Charl <charldesign@gmIl.com>
2019-08-11 15:20:29 +0200

    first commit

---
+++ mobile/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { TextInput, StyleSheet, Text, View, Button } from "react-native";
+import Driver from "./screens/Driver";
+import Passenger from "./screens/Passenger";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDriver: false,
+      isPassenger: false
+    };
+  }
+
+  render() {
+    if (this.state.isDriver) {
+      return <Driver />;
+    }
+
+    if (this.state.isPassenger) {
+      return <Passenger />;
+    }
+
+    return (
+      <View style={styles.container}>
+        <Button
+          onPress={() => this.setState({ isPassenger: true })}
+          title="Passenger"
+        />
+        <Button
+          onPress={() => this.setState({ isDriver: true })}
+          title="Driver"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50
+  }
+});
---
+++ mobile/google_api_key.js
@@ -0,0 +1,3 @@
+const apiKey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+
+module.exports = apiKey;
